c18b65f1304f4134337cdca937c671f962de0cb2
felixyh <felix_yanghui@sina.cn>
2022-12-31 19:05:06 +0800

    vsphere-terraform

--- s3.py
+++ s3.py
@@ -4,9 +4,21 @@ import boto3
 # s3 = boto3.resource('s3')
 
 # for bucket in s3.buckets.all():
-#     print(bucket.name)
+#     print(bucket.name, bucket.creation_date)
 
-iam = boto3.client('iam')
+# s3 = boto3.client('s3')
+# response = s3.list_buckets()
+
+# print('Existing buckets and objects')
+# bucketname = []
+# for bucket in response['Buckets']:
+#     bucketname.append(bucket["Name"])
+# for bucket in bucketname:
+#     response = s3.list_objects_v2(Bucket=bucket)
+#     print(bucket, 'has objects:' , [content['Key'] + ' size: ' + str(content['Size']) for content in response['Contents']])
+
+
+# iam = boto3.client('iam')
 
 
 # response = iam.create_user(
@@ -21,6 +33,15 @@ iam = boto3.client('iam')
 # for response in paginator.paginate():
 #     print(response)
 
-iam.delete_user(
-    UserName='Valaxy-Demo-User'
-)
+# iam.delete_user(
+#     UserName='Valaxy-Demo-User'
+# )
+
+
+s3 = boto3.resource('s3')
+
+# s3.Bucket(name=bucket).objects.all()
+
+for bucket in s3.buckets.all():
+    for object in bucket.objects.all():
+        print(bucket.name, object.key, object.size)
